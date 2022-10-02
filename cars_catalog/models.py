0165ffa15dc82be8943d77b9f91e53e2059b0883
MetaloNN <herrera.a.nicolas@gmail.com>
2022-10-02 13:49:43 -0300

    Add a class Car into app/models.py

--- cars_catalog/models.py
+++ cars_catalog/models.py
@@ -1,3 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from enum import unique
 
 # Create your models here.
+
+class Car(models.Model):
+    palte = models.CharField(max_lenght = 6 , unique = True)
+    brand = models.CharField(max_lenght = 30)
+    model = models.CharField(max_lenght = 30)
+    color = models.CharField(max_lenght = 20)
+    transmission = models.CharField(max_lenght = 8)
