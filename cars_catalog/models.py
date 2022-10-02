1a298a7fc168ba2a046044b4d89a15d6d1d972e9
MetaloNN <herrera.a.nicolas@gmail.com>
2022-10-02 13:55:43 -0300

    Modify max_lenght for max_length into app/models.py

--- cars_catalog/models.py
+++ cars_catalog/models.py
@@ -1,12 +1,11 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from enum import unique
 
 # Create your models here.
 
 class Car(models.Model):
-    palte = models.CharField(max_lenght = 6 , unique = True)
-    brand = models.CharField(max_lenght = 30)
-    model = models.CharField(max_lenght = 30)
-    color = models.CharField(max_lenght = 20)
-    transmission = models.CharField(max_lenght = 8)
+    palte = models.CharField(max_length = 6 , unique = True)
+    brand = models.CharField(max_length = 30)
+    model = models.CharField(max_length = 30)
+    color = models.CharField(max_length = 20)
+    transmission = models.CharField(max_length = 8)
